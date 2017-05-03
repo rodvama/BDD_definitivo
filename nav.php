d2a36383cc354b805716da7b0534edca98fd6883
ValentinHuerta <Vale@valentins-MacBook-Pro.local>
2017-05-03 13:47:12 -0500

    Links

--- nav.php
+++ nav.php
@@ -6,8 +6,8 @@
     <li> <div aria-label="Left Align" class="Notifications">
     <span onclick="location.href='index.html';" class="glyphicon glyphicon-home" aria-hidden="true"><c class="tooltiptext">Inicio</c></span>
     <span onclick="location.href='dashboard.php';" class="glyphicon glyphicon-dashboard" aria-hidden="true"><c class="tooltiptext">Dashboard</c></span>
-    <span onclick="location.href='descubrefinal.php';" class="glyphicon glyphicon-globe" aria-hidden="true"><c class="tooltiptext">Descubre</c></span>
-   <span onclick="location.href='crear.php';" class="glyphicon glyphicon-plus" aria-hidden="true"><c class="tooltiptext">Crear Proyecto</c></span>
+    <span onclick="location.href='descubrefinal.html';" class="glyphicon glyphicon-globe" aria-hidden="true"><c class="tooltiptext">Descubre</c></span>
+   <span onclick="location.href='proyectos.php';" class="glyphicon glyphicon-plus" aria-hidden="true"><c class="tooltiptext">Crear Proyecto</c></span>
     <a class="glyphicon glyphicon-search" href="#search"><c class="tooltiptext">Búsqueda</c></a>
     <span class="glyphicon glyphicon-bell" aria-hidden="true"><c class="tooltiptext">Notificaciones</c></span>
     </div>
--- sidebar.php
+++ sidebar.php
@@ -5,7 +5,7 @@
         <!-- <li onclick="location.href='calendar.php';"><span class="glyphicon glyphicon-calendar"  aria-hidden="true"></span> <b> &nbsp; Calendario </b></li> D-->
         <li onclick="location.href='documents.php';"><span class="glyphicon glyphicon-folder-open" aria-hidden="true"></span> <b> &nbsp; Documentos </b></li>
         <li onclick="location.href='sessions.php';"><span class="glyphicon glyphicon-pushpin" aria-hidden="true"></span> <b> &nbsp; Sesiones </b></li>
-        <li onclick="location.href='descubrefinal.php';"><span class="glyphicon glyphicon-comment" aria-hidden="true"></span> <b> &nbsp; Social </b></li>
+        <li onclick="location.href='descubrefinal.html';"><span class="glyphicon glyphicon-comment" aria-hidden="true"></span> <b> &nbsp; Social </b></li>
     </ul>
 <div> </div> </div>
 <!--Side Menu END-->
